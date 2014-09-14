551701c6736f633b4afe024646ace02309625122
antnasce <ant@Mac.home>
2014-09-14 16:07:16 +0100

    Completing challenge 1 and some tweaks of storyboard

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,11 +15,13 @@ class ViewController: UIViewController {
   // Optionals it either exists or it doesn't
   // It makes sense that they exist because they've been connected up from IB
   // See http://commandshift.co.uk/blog/2014/07/20/swift-implicitly-unwrapped-optionals/
+  
   @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var enterNameTextField: UITextField!
   @IBOutlet weak var enterMessageTextField: UITextField!
   @IBOutlet weak var sendMailButton: UIButton!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -34,12 +36,21 @@ class ViewController: UIViewController {
   
     // Code will evaluate on touch up inside event sent to UIButton
     messageLabel.text = enterMessageTextField.text
+    nameLabel.text = enterNameTextField.text
     messageLabel.hidden = false
+    nameLabel.hidden = false
+    
     enterMessageTextField.text = ""
-    enterMessageTextField.resignFirstResponder()
-    messageLabel.textColor = UIColor.greenColor()
+    enterNameTextField.text = ""
+    
+    messageLabel.textColor = UIColor.redColor()
+    nameLabel.textColor = UIColor.blueColor()
+
     sendMailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     sendMailButton.backgroundColor = UIColor.greenColor()
+
+    enterMessageTextField.resignFirstResponder()
+    enterNameTextField.resignFirstResponder()
   }
 
 }
